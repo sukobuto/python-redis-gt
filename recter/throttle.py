18dd819306c1f67620f68227a793de461abf7bb1
Kenta Suzuki <k-suzuki@mi-labo.co.jp>
2018-11-10 22:12:07 +0900

    implement throttling

--- recter/throttle.py
+++ recter/throttle.py
@@ -1,14 +1,36 @@
 # from typing import *
+import asyncio
+from datetime import datetime
+
 from redis import StrictRedis
+from uuid import uuid4
 
 
 class Throttle:
 
-    def __init__(self, redis: StrictRedis):
+    def __init__(self, redis: StrictRedis, max_parallels: int):
         self.redis = redis
+        self.max_parallels = max_parallels
 
-    def set_string(self):
-        self.redis.set('t-key', 't-value'.encode('utf8'))
+    @property
+    def __key(self):
+        return 'recter'
 
-    def get_string(self):
-        return self.redis.get('t-key').decode('utf8')
+    async def run_async(self, coroutine):
+        # return await coroutine
+        key = self.__key
+        token = str(uuid4()).encode('utf8')
+        timestamp = datetime.now().timestamp()
+        self.redis.zadd(key, timestamp, token)
+        limit = 100
+        while True:
+            cleared = self.redis.zrange(key, 0, self.max_parallels - 1)
+            if token in cleared:
+                break
+            limit -= 1
+            if limit <= 0:
+                raise Exception('retry limit over')
+            await asyncio.sleep(0.05)
+        result = await coroutine
+        self.redis.zrem(key, token)
+        return result
--- tests/test_throttle.py
+++ tests/test_throttle.py
@@ -1,14 +1,58 @@
+import asyncio
+from datetime import datetime
+from pprint import pprint
+
+import pytest
 from fakeredis import FakeStrictRedis
-from redis import StrictRedis
 from recter.throttle import Throttle
 
 
-class TestThrottle:
+@pytest.fixture(scope="module", autouse=True)
+def redis():
+    return FakeStrictRedis()
 
-    def setup_method(self):
-        self.r: StrictRedis = FakeStrictRedis()
 
-    def test_run(self):
-        throttle = Throttle(self.r)
-        throttle.set_string()
-        assert throttle.get_string() == 't-value'
+def test_fakeredis_zrem(redis):
+    redis.zadd('test', 1.1, 'AAA')
+    print(redis.zrange('test', 0, -1))
+    assert redis.zrem('test', 'AAA') == 1
+    assert redis.zrem('test', 'AAA') == 0
+    print(redis.zrange('test', 0, -1))
+    assert redis.zrange('test', 0, -1) == []
+
+
+def test_throttle_runs_coroutine(redis, event_loop: asyncio.AbstractEventLoop):
+    throttle = Throttle(redis, 1)
+    done = False
+
+    async def do_something(a, b):
+        nonlocal done
+        await asyncio.sleep(0.1)
+        done = True
+        return a * b
+
+    coroutine = do_something(3, 7)
+    assert not done
+    assert event_loop.run_until_complete(throttle.run_async(coroutine)) == 21
+    assert done
+
+
+def test_throttle_works_with_para1(redis, event_loop: asyncio.AbstractEventLoop):
+    throttle = Throttle(redis, 1)
+    start_at = datetime.now().timestamp()
+    start_logs = []
+
+    async def do_something():
+        nonlocal start_logs
+        start_logs.append((datetime.now().timestamp() - start_at))
+        await asyncio.sleep(0.1)
+
+    tasks = [throttle.run_async(do_something()) for n in range(5)]
+    event_loop.run_until_complete(asyncio.wait(tasks))
+
+    start_logs = list(sorted(start_logs))
+    pprint(start_logs)
+    last_elapsed = start_logs[0]
+    for elapsed in start_logs[1:]:
+        assert elapsed - last_elapsed > 0.09
+        last_elapsed = elapsed
